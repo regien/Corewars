2a55f2ecdecbb43e49a7cce4752b16b8ba6a6aa0
Alex Dubugras <alexdubugras@gmail.com>
2018-05-16 13:58:38 -0700

    message to Gerardo

--- src/corewar.c
+++ src/corewar.c
@@ -6,7 +6,7 @@
 /*   By: adubugra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 23:23:05 by adubugra          #+#    #+#             */
-/*   Updated: 2018/05/16 01:11:28 by adubugra         ###   ########.fr       */
+/*   Updated: 2018/05/16 13:58:25 by adubugra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int main(int argc, char **argv)
 	
 	if (argc < 2)
 		return (0);
-	vm.players = argc - 1; //test
-	set_champs(&(vm.champs[0]), argv[1]);
-	set_champs(&(vm.champs[1]), argv[2]);
+
+	vm.players = argc - 1; //Gerardo, please replace these 3 lines for
+	set_champs(&(vm.champs[0]), argv[1]);//the function that checks args and 
+	set_champs(&(vm.champs[1]), argv[2]);//sets the fd and num_of_players
 	if (!read_files(vm.players, &vm))
 		return (1);
 }
