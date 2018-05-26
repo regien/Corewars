faf73b435f9161d20c716e494ac247737753f037
Alex Dubugras <alexdubugras@gmail.com>
2018-05-25 17:48:50 -0700

    doesnt accepted wrong args

--- src/asm_op_builder.c
+++ src/asm_op_builder.c
@@ -6,11 +6,13 @@
 /*   By: adubugra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 18:33:23 by adubugra          #+#    #+#             */
-/*   Updated: 2018/05/25 17:01:19 by adubugra         ###   ########.fr       */
+/*   Updated: 2018/05/25 17:40:37 by adubugra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/asm.h"
+#define OP_TABLE op_table[op->op_code - 1]
+#define ARG_NAME op->arg_name[i]
 
 int		set_op_name(char **strreal, t_ops *op)
 {
@@ -57,18 +59,31 @@ int		set_arguments(char **strreal, t_ops *op)
 	return (0);
 }
 
-void	set_bit_descriptor(char *descriptor, char *arg_name, int arg_num)
+int		set_bit_descriptor(t_ops *op, char *arg_name, int arg_num)
 {
-	char d;
+	char	d;
+	char	descriptor;
 
-	if (arg_name[0] == 'r')
-		d = 1;
-	else if (arg_name[0] == DIRECT_CHAR)
+	descriptor = op->descriptor;
+	d = arg_name[0] == 'r' ? 1 : 0;
+	if (arg_name[0] == 'r' &&
+			(!(OP_TABLE.accepted_args[arg_num - 1] & T_REG)))
+		return (ft_printf_err("invalid arg type reg\n"));
+	else if (arg_name[0] != 'r' && arg_name[0] == DIRECT_CHAR)
+	{
+		if (!(OP_TABLE.accepted_args[arg_num - 1] & T_DIR))
+			return (ft_printf_err("invalid arg type dir\n"));
 		d = 2;
-	else
+	}
+	else if (arg_name[0] != 'r')
+	{
+		if (!(OP_TABLE.accepted_args[arg_num - 1] & T_IND))
+			return (ft_printf_err("invalid arg type ind\n"));
 		d = 3;
+	}
 	d = d << (8 - (2 * arg_num));
-	*descriptor = *descriptor | d;
+	op->descriptor = descriptor | d;
+	return (0);
 }
 
 int		get_arguments_and_sizes(t_ops *op)
@@ -80,13 +95,13 @@ int		get_arguments_and_sizes(t_ops *op)
 	{
 		if (check_argument(op->arg_name[i]))
 			return (ft_printf_err("parse error: %s\n", op->arg_name[i]));
-		set_bit_descriptor(&(op->descriptor), op->arg_name[i], i + 1);
-		op->label_arg[i] = (op->arg_name[i][0] == LABEL_CHAR ||
-				op->arg_name[i][1] == LABEL_CHAR) ? 1 : 0;
+		if (set_bit_descriptor(op, op->arg_name[i], i + 1))
+			return (1);
+		op->label_arg[i] = (ARG_NAME[0] == LABEL_CHAR ||
+				ARG_NAME[1] == LABEL_CHAR) ? 1 : 0;
 		op->args[i] = ft_atoi(&(op->arg_name[i][1]));
-		if (op->arg_name[i][0] == 'r')
-			op->arg_sizes[i] = 1;
-		else if (op->arg_name[i][0] == DIRECT_CHAR)
+		op->arg_sizes[i] = op->arg_name[i][0] == 'r' ? 1 : 0;
+		if (op->arg_name[i][0] == DIRECT_CHAR)
 			op->arg_sizes[i] = op_table[(int)op->op_code - 1].truncate ? 2 : 4;
 		else if (ft_isdigit_sign(op->arg_name[i][0]) ||
 				op->arg_name[i][0] == LABEL_CHAR)
@@ -94,7 +109,7 @@ int		get_arguments_and_sizes(t_ops *op)
 			op->args[i] = ft_atoi(&(op->arg_name[i][0]));
 			op->arg_sizes[i] = 2;
 		}
-		else
+		else if (op->arg_sizes[i] == 0)
 			return (ft_printf_err("unknow argument type\n"));
 	}
 	return (0);
